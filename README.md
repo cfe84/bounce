6035ccff12fd56d3428bdcfae8d0a1078d8e5175
Charles Feval <chfeval@microsoft.com>
2018-04-12 10:08:29 -0700

    Bounce

---
+++ README.md
@@ -0,0 +1,80 @@
+Picture it: you're running through the forest with zombie
+donkeys right behind you. They've been hunting you the whole
+day, and it's already been 45 minutes of straight-up
+sprint. You're exhausted. Suddenly, you need a very simple
+local http server that just echoes what you're doing and
+tells you what it sees.
+
+Well, fear no more Dresdel, because I've got you covered.
+
+This is a server that will listen to HTTP calls and
+answer whatever you want it to.
+
+## Install
+
+```
+npm install -g bounce-server
+```
+
+## Use
+
+```sh
+bounce --get /api/users/:id/something --post /api/users/ --port 8080 --echo &
+
+# Create endpoint GET /api/users/:id/something
+# Create endpoint POST /api/users/
+# Listening on port 8080
+
+curl --header "thisis:a header" http://localhost:8080/api/users/123/something?this_is=a_query_parameter
+
+# GET /api/users/:id/something
+# Headers: {
+#   "host": "localhost:8080",
+#   "user-agent": "curl/7.47.0",
+#   "accept": "*/*",
+#   "thisis": "a header"
+# }
+# Query: {
+#   "this_is": "a_query_parameter"
+# }
+# Params: {
+#   "id": "123"
+# }
+# Body:
+
+------------------------------
+
+curl --header "thisis:a header" -X POST http://localhost:8080/api/users/ --data "this is the body"
+
+# POST /api/users/
+# Headers: {
+#   "host": "localhost:8080",
+#   "user-agent": "curl/7.47.0",
+#   "accept": "*/*",
+#   "thisis": "a header",
+#   "content-length": "16",
+#   "content-type": "application/x-www-form-urlencoded"
+# }
+# Query: {}
+# Params: {}
+# Body: this is the body
+
+
+# ------------------------------
+# this is the body ### <- this is curl's output
+```
+
+## Parameters
+
+```
+  -g, --get /relative/url/:optional_parameter/      create a GET endpoint
+  -u, --put /relative/url/:optional_parameter/      create a PUT endpoint
+  -p, --post /relative/url/:optional_parameter/     create a POST endpoint
+  -d, --delete /relative/url/:optional_parameter/   create a DELETE endpoint
+  -e, --echo                                        reply with request body
+  -b, --response response body                      specify response to be sent
+  -s, --status http status                          specify status for response. Defaults to 200
+  -P, --port port number                            port to listen to. Defaults to environment variable PORT, then
+                                                    8080
+  -h, --help                                        display a help message
+```
--- server.js
+++ server.js
@@ -1,10 +1,80 @@
 const express = require("express");
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
+
+const exampleUrl = "{underline /relative/url/:optional_parameter/}"
+const optionDefinitions = [
+    { name: 'get', typeLabel: exampleUrl, description: "create a GET endpoint", alias: "g", type: String, multiple: true},
+    { name: 'put', typeLabel: exampleUrl, description: "create a PUT endpoint", alias: "u", type: String, multiple: true},
+    { name: 'post', typeLabel: exampleUrl, description: "create a POST endpoint", alias: "p", type: String, multiple: true},
+	{ name: 'delete', typeLabel: exampleUrl, description: "create a DELETE endpoint", alias: "d", type: String, multiple: true},
+	{ name: 'echo', description: "reply with request body", alias: "e", type: Boolean},
+	{ name: 'response', typeLabel: "{underline response body}", description: "specify response to be sent", alias: "b", type: String},
+	{ name: 'status', typeLabel: "{underline http status}", description: "specify status for response. Defaults to 200", alias: "s", type: Number},
+	{ name: 'port', typeLabel: "{underline port number}", description: "port to listen to. Defaults to environment variable PORT, then 8080", alias: "P", type: Number},
+	{ name: 'help', description: "display this message", alias: "h", type: Boolean},
+];
+
+const methods = [
+	"get",
+	"put",
+	"post",
+	"delete"
+];
+
+const args = commandLineArgs(optionDefinitions, process.argv);
+if (process.argv.length === 2 || args.help) {
+	const usage = [
+		{ 
+			header: "Bounce", 
+			content: "This is a server that will listen to HTTP calls and \
+answer what you ask it to."},
+		{
+			header: "Example",
+			content: "bounce --get /api/users/:id/something --post /api/users/ --port 8080 --echo &"
+		},
+		{
+			header: "Options",
+			optionList: optionDefinitions
+		}
+	]
+	console.log(commandLineUsage(usage));
+	return;
+}
+
 const app = express();
 
-app.get("/MSI/token/", (req, res) => {
-	console.log("received call");
-	res.write("This is me");
-	res.end();
+const echo = args.echo;
+const response = args.response;
+const PORT = args.port || process.env.PORT || 8080;
+const status = args.status || 200;
+
+methods.forEach((method) => {
+	if (args[method]) {
+		args[method].forEach((endpoint) => {
+			console.log(`Create endpoint ${method.toUpperCase()} ${endpoint}`);
+			app[method](endpoint, (req, res) => {
+				console.log(`\n\n${method.toUpperCase()} ${endpoint}`);
+				console.log(`Headers: ${JSON.stringify(req.headers, null, 2)}`);
+				console.log(`Query: ${JSON.stringify(req.query, null, 2)}`);
+				console.log(`Params: ${JSON.stringify(req.params, null, 2)}`);
+				res.statusCode = status;
+				let data = "";
+				req.on("data", (chunk) => data += chunk);
+				req.on("end", () => { 
+					if (echo) {
+						res.write(data);
+					}
+					if (response) {
+						res.write(response);
+					}
+					res.end();
+					console.log(`Body: ${data}`);
+					console.log(`\n\n------------------------------`);
+				});
+			});			
+		})		
+	}
 });
 
-app.listen(8080);
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
